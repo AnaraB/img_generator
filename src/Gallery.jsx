e5d8e71f3eddd8e8e053ad1afd43cdb454cc0541
Anara <berdali.aygerim9@gmail.com>
2025-03-27 20:23:14 +0400

    render img, react query devtools installed

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-// const url = 'https://api.unsplash.com/search/photos?page=3&query=dog&client_id=${key}'
 
+const url = `https://api.unsplash.com/search/photos?page=3&query=berry&client_id=${key}`;
 
 function Gallery() {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading..</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error..</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
   return (
-    <div>Gallery</div>
-  )
+    <section className="image-container">
+      {results.map((item) => {
+        //use optional chaiing in case property is not there
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 }
 
-export default Gallery
+export default Gallery;
