0919e52265aabc063e487d983c202e538f43c47c
Anara <berdali.aygerim9@gmail.com>
2025-03-27 16:14:25 +0400

    Project set up, context set up

---
+++ src/ThemeToggle.jsx
@@ -0,0 +1,12 @@
+// import React, { useEffect } from "react"
+import { useGlobalContext } from "./context"
+
+const  ThemeToggle = ()=> {
+  const { greeting } = useGlobalContext();
+  console.log(greeting);
+
+  return <h2> ThemeToggle</h2>
+  
+}
+
+export default ThemeToggle
